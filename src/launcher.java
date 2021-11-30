a8342260957722db28e7cd01bd98492804b14d86
Ouma4 <bouyahy@et.esiea.fr>
2021-11-30 10:16:44 +0100

    add launcher part 4

--- src/launcher.java
+++ src/launcher.java
@@ -12,7 +12,30 @@ public class launcher {
         while (!x.equals("quit")){
             System.out.println("Unknown command");
         }
+
+        //The Fibonacci
+        System.out.println("*******");
+        System.out.println("suite de Fibonacci ");
+        System.out.println("Fibo :donner un nombre n");
+        Scanner N = new Scanner(System.in);
+        //We should have an integer
+        int c = Integer.parseInt("N");
+        //c = input.nextInt();
+        int n1 = 0,n2 = 1,n3 = 0;
+        if (c > 0){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.println("F(0) = "+n1);
+        }else{
+            
+        }
+
+
+
+
     }
 
 }
+
 
