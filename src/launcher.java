007aed525ac8528e939db5a19fecc78687f59663
Ouma4 <bouyahy@et.esiea.fr>
2021-11-30 09:23:57 +0100

    add launcher

---
+++ src/launcher.java
@@ -0,0 +1,18 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class launcher {
+
+    public static void main(String[] args) {
+
+        System.out.println("bienvenue");
+        System.out.println("Put a variable ^^");
+        Scanner newVariable = new Scanner(System.in);
+        String x = newVariable.nextLine();
+        if(!x.equals("quit" )){
+            System.out.println("Unknown");
+        }
+    }
+
+}
+
